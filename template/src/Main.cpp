82a6ba2f1cac0c330f15caff997cdd1bda1b1e13
Hazlan Qodrey <57006944+hzlnqodrey@users.noreply.github.com>
2021-03-08 18:47:13 +0700

    Add files via upload

---
+++ template/src/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a;
+    cout << "Hello World!" << endl;
+    cout << 2 + 3 << endl;
+    cin >> a;
+    cout << a << endl;
+ 
+    return 0;
+}
