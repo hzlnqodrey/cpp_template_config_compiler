de8cff75bf1a317f5aaf7d960190b89fa42845de
Hazlan Qodrey <57006944+hzlnqodrey@users.noreply.github.com>
2021-03-12 00:31:14 +0700

    Update Main.cpp

--- template/src/Main.cpp
+++ template/src/Main.cpp
@@ -3,11 +3,8 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int a;
+    
     cout << "Hello World!" << endl;
-    cout << 2 + 3 << endl;
-    cin >> a;
-    cout << a << endl;
- 
+    
     return 0;
 }
